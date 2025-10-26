2f241ebc8677a40b009d3ca3bf0e0d9acedf9218
Jasperae <igweohelume@gmail.com>
2025-10-26 21:02:29 +0100

    Update shoppingsummarycounter.go

--- Hackathon/Hour 7/shoppingsummarycounter/shoppingsummarycounter.go
+++ Hackathon/Hour 7/shoppingsummarycounter/shoppingsummarycounter.go
@@ -1,5 +1,27 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
+	counts := make(map[string]int)
+	wordStarted := false
+	word := ""
 
+	for i := 0; i < len(str); i++ {
+		char := str[i]
+		if char != ' ' {
+			word += string(char)
+			wordStarted = true
+		} else {
+			if wordStarted {
+				counts[word]++
+				word = ""
+				wordStarted = false
+			}
+		}
+	}
+
+	if wordStarted {
+		counts[word]++
+	}
+
+	return counts
 }
