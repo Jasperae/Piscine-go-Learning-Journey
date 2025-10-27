e99e897bc39033b41de158cba4639f6a17f9d025
ugochi74 <confidenceodo4@gmail.com>
2025-10-27 20:08:14 +0100

    Move SortWordArr logic into its own function
    Refactor main function to SortWordArr function.

--- Quest 9/sortwordarr/main.go
+++ Quest 9/sortwordarr/main.go
@@ -1,13 +1,11 @@
-package main
+package piscine
 
-import (
-	"fmt"
-	"piscine"
-)
-
-func main() {
-	result := []string{"a", "A", "1", "b", "B", "2", "c", "C", "3"}
-	piscine.SortWordArr(result)
-
-	fmt.Println(result)
+func SortWordArr(a []string) {
+	for i := 0; i < len(a)-1; i++ {
+		for j := i + 1; j < len(a); j++ {
+			if a[i] > a[j] {
+				a[i], a[j] = a[j], a[i]
+			}
+		}
+	}
 }
