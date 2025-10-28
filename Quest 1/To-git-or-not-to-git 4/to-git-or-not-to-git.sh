67af23008cb7d83412b80f802cf8dd63579e6819
Jasperae <igweohelume@gmail.com>
2025-10-28 14:26:15 +0100

    Update to-git-or-not-to-git.sh

--- Quest 1/To-git-or-not-to-git 4/to-git-or-not-to-git.sh
+++ Quest 1/To-git-or-not-to-git 4/to-git-or-not-to-git.sh
@@ -1,25 +1,3 @@
-package piscine
+#! /bin/bash 
 
-import "github.com/01-edu/z01"
-
-func PrintComb2() {
-	for i := 0; i <= 98; i++ {
-		for j := i + 1; j <= 99; j++ {
-			// Print first number
-			z01.PrintRune(rune('0' + i/10))
-			z01.PrintRune(rune('0' + i%10))
-			z01.PrintRune(' ')
-
-			// Print second number
-			z01.PrintRune(rune('0' + j/10))
-			z01.PrintRune(rune('0' + j%10))
-
-			// Print separator (comma and space) unless it's the last combination
-			if i != 98 || j != 99 {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
-			}
-		}
-	}
-	z01.PrintRune('\n')
-}
+curl https://learn.01founders.co/assets/superhero/all.json | jq '.[] | select(.id == 170) | .name, .powerstats.power, .appearance.gender' -r
